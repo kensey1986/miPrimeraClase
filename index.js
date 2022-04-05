45b60576c6c350b7d874d503e335429ef8fc66b6
Yivan2021 <manuel.rodriguez@softwareestrategico.com>
2022-04-04 21:44:39 -0500

    explique-tipos-de-datos

---
+++ index.js
@@ -0,0 +1,22 @@
+// let miDato;
+// miDato= 'hola esto es un string';
+
+let miDato = 'hola esto es un string- cadena de texto';
+console.log("🚀 ~ file: index.js ~ line 5 ~ miDato--->>", miDato);
+//tipo string
+miDato = 5.500;
+console.log("🚀 ~ file: index.js ~ line 7 ~ miDato--->>", miDato)
+
+//tipo boolean
+miDato =true;
+console.log("🚀 ~ file: index.js ~ line 11 ~ miDato--->>", miDato)
+
+// tipo objeto
+miDato = { nombre: 'pepe', edad: 55, civil: false}; 
+console.log("🚀 ~ file: index.js ~ line 15 ~ miDato --->", miDato);
+// tipo undefined
+miDato = undefined;
+console.log("🚀 ~ file: index.js ~ line 18 ~ miDato --->", miDato)
+// tipo null
+miDato = null;
+console.log("🚀 ~ file: index.js ~ line 21 ~ miDato", miDato)
